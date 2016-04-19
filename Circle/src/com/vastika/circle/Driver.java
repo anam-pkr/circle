3511046855744e2baf41c9d7a462aa7fd50e5875
Anam Raj Sharma <anam.pkr@gmail.com>
2016-04-19 09:48:10 -0700

    initial checkin of circle

---
+++ Circle/src/com/vastika/circle/Driver.java
@@ -0,0 +1,21 @@
+package com.vastika.circle;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+public class Driver {
+	public static void main(String[] args) {
+		Circle c8 = new Circle();
+		c8.radius=44;
+		Circle c1 = new Circle(1);
+		c1.setRadius(5);
+		c1.radius= 3;
+		Circle c2 = new Circle(5);
+		Circle c3 = new Circle(3);
+		double b1 = c1.getArea();
+		double b2 = c2.getArea();
+		double b3 = c3.getArea();
+		System.out.println(b1 + " " + b2 +" "+ b3);
+ c1.setRadius(5);
+System.out.println();
+	}
+}
